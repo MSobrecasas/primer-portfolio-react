b9c589811a5e620623ef73dc04c0194c282bedf2
MSobrecasas <sns.mary@hotmail.com>
2022-07-28 19:53:05 -0300

    agregando cambios

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,67 @@
 import React from "react";
 import "./Contact.css";
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { ImWhatsapp } from "react-icons/im";
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
 
 const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_4ewzs1e', 'template_moda2cp', form.current, 'fVmDRCFJU8asVr1Eh')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+            e.target.reset();
+    };
+
     return (
         <section id="contact" className="contact">
-            <h1>Contact</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam, quidem.
-            </p>
+            <h5>Get In Touch</h5> 
+            <h2>Contact Me</h2>
+
+            <div className="container contact__container">
+
+                <div className="contact__options">
+                    <article className="contact__option">
+                        <MdOutlineAlternateEmail className="contact__option-icon" />
+                        <h4>Email</h4>
+                        <h5>email@email.com</h5>
+                        <a href="mailto::email@email.com" target="_blank">Send a message</a>
+                    </article>
+
+                    <article className="contact__option">
+                        <RiMessengerLine className="contact__option-icon" />
+                        <h4>Messenger</h4>
+                        <h5>Usuario Facebbok</h5>
+                        <a href="https://m.me/usuario.facebook" target="_blank">Send a message</a>
+                    </article>
+
+                    <article className="contact__option">
+                        <ImWhatsapp className="contact__option-icon" />
+                        <h4>Whatsapp</h4>
+                        <h5>+5444444</h5>
+                        <a href="https://api.whatsapp.con/send?phone+544444" target="_blank">Send a message</a>
+                    </article>
+                </div>
+
+                <form ref={form} onSubmit={sendEmail} >
+                    <input type="text" name="name" placeholder="Your Full Name" required />
+                    <input type="email" name="email" placeholder="Your Email" required />
+                    <textarea name="message" placeholder="Your Message" rows="7" required />
+                    <button type="submmit" className="btn btn-primary">Send Message</button>
+                </form>
+
+            </div>
         </section>
     );
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,56 +2,66 @@ import React from "react";
 import "./Testimonials.css";
 import AVTR1 from '../../assets/cheems.jpg'
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+    {
+        avatar: AVTR1,
+        name: "Cheems",
+        review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos aliquid nam dicta asperiores eligendi reprehenderit ea repellat et possimus dolore, sit repellendus quibusdam dolorem maiores, aspernatur quaerat. Maxime, ducimus optio!'
+    },
+    {
+        avatar: AVTR1,
+        name: "Cheems",
+        review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos aliquid nam dicta asperiores eligendi reprehenderit ea repellat et possimus dolore, sit repellendus quibusdam dolorem maiores, aspernatur quaerat. Maxime, ducimus optio!'
+    },
+    {
+        avatar: AVTR1,
+        name: "Cheems",
+        review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos aliquid nam dicta asperiores eligendi reprehenderit ea repellat et possimus dolore, sit repellendus quibusdam dolorem maiores, aspernatur quaerat. Maxime, ducimus optio!'
+    },
+    {
+        avatar: AVTR1,
+        name: "Cheems",
+        review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos aliquid nam dicta asperiores eligendi reprehenderit ea repellat et possimus dolore, sit repellendus quibusdam dolorem maiores, aspernatur quaerat. Maxime, ducimus optio!'
+    },
+]
+
 
 const Testimonials = () => {
     return (
         <section id="testimonials" className="testimonials">
             <h5>Review from clients</h5>
             <h2>Testimonial</h2>
-            <div className="container testimonial__container">
-                <article className="testimonial">
-                    <div className="client__avatar">
-                        <img src={AVTR1} alt=" Avatar" />
-                    </div>
-                    <h5 className="client__name"> Cheems </h5>
-                    <small className="client__review">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Dignissimos aliquid nam dicta asperiores eligendi
-                        reprehenderit ea repellat et possimus dolore,
-                        sit repellendus quibusdam dolorem maiores, aspernatur quaerat.
-                        Maxime, ducimus optio!
-                    </small>
-                </article>
+            <Swiper className="container testimonial__container"
+            modules={[Pagination]}
+            spaceBetween={400}
+            slidesPerView={1}
+            pagination={{ clickable: true }}>
+                {
+                    data.map(({ avatar, name, review }, index) => {
+                        return (
+                            <SwiperSlide key={index} className="testimonial">
+                                <div className="client__avatar">
+                                    <img src={avatar} alt=" Avatar" />
+                                </div>
+                                <h5 className="client__name"> {name} </h5>
+                                <small className="client__review">
+                                  {review}
+                                </small>
+                            </SwiperSlide>
+                        )
+                    })
+                }
 
-                <article className="testimonial">
-                    <div className="client__avatar">
-                        <img src={AVTR1} alt=" Avatar" />
-                    </div>
-                    <h5 className="client__name"> Cheems </h5>
-                    <small className="client__review">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Dignissimos aliquid nam dicta asperiores eligendi
-                        reprehenderit ea repellat et possimus dolore,
-                        sit repellendus quibusdam dolorem maiores, aspernatur quaerat.
-                        Maxime, ducimus optio!
-                    </small>
-                </article>
-
-                <article className="testimonial">
-                    <div className="client__avatar">
-                        <img src={AVTR1} alt=" Avatar" />
-                    </div>
-                    <h5 className="client__name"> Cheems </h5>
-                    <small className="client__review">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Dignissimos aliquid nam dicta asperiores eligendi
-                        reprehenderit ea repellat et possimus dolore,
-                        sit repellendus quibusdam dolorem maiores, aspernatur quaerat.
-                        Maxime, ducimus optio!
-                    </small>
-                </article>
-
-            </div>
+            </Swiper>
         </section>
     );
 }
